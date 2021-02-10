338efbf695daf9b8d91c05927176863b0f5c0aea
Giovanni Kurniawan <giovanni.kurniawan@student.umn.ac.id>
2021-02-10 20:03:24 +0700

    menambahkan tugas

---
+++ Week01_28665/app/src/main/java/umn/ac/id/week01_28665/MainActivity.java
@@ -0,0 +1,13 @@
+package umn.ac.id.week01_28665;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
